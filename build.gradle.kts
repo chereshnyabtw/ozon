ebdc28724581552e58aef85e5b29fff2a397d73b
Artemiy Chereshnevvv <chereshnyabtw@icloud.com>
2022-10-24 15:40:54 +0500

    getter for services

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ publishing {
 		create<MavenPublication>("maven") {
 			groupId = "com.github.chereshnyabtw"
 			artifactId = "ozon"
-			version = "1.0.0"
+			version = "1.1.0"
 
 			from(components["kotlin"])
 		}
--- src/main/kotlin/ru/chrshnv/ozonapi/OzonClient.kt
+++ src/main/kotlin/ru/chrshnv/ozonapi/OzonClient.kt
@@ -1,8 +1,18 @@
 package ru.chrshnv.ozonapi
 
 import ru.chrshnv.ozonapi.config.Settings
+import ru.chrshnv.ozonapi.services.AnalyticsService
+import ru.chrshnv.ozonapi.services.StocksService
 
-class OzonClient(private val clientId: String, private val accessToken: String) {
+class OzonClient(private val clientId: String, private val accessToken: String, private val analyticsService: AnalyticsService = AnalyticsService(), private val stocksService: StocksService = StocksService()) {
+	fun getAnalyticsService(): AnalyticsService {
+		return analyticsService
+	}
+
+	fun getStocksService(): StocksService {
+		return stocksService
+	}
+
 	fun setup() {
 		Settings.getInstance().clientId = clientId
 		Settings.getInstance().accessToken = accessToken
